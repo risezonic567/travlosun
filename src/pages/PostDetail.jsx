38a384e437a03e6892cfb9334ec773d18ddccb2e
Babloothk08 <babloothakur42021@gmail.com>
2026-02-13 17:56:11 +0530

    canonical tag updated

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -27,6 +27,7 @@ export default function PostDetail() {
       <Helmet>
         <title>{post.metatitle}</title>
         <meta name="description" content={post.metadescription} />
+        <link rel="stylesheet" href={post.canonical} />
         <meta name="keywords" content={post.metakeyword} />
 
       </Helmet>
